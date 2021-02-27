e37650bc3850527e060d81ee7e29703f342dad53
Paulo <paulo_sanches2012@hotmail.com>
2021-02-27 15:17:23 -0300

    custom errors

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,28 @@
 import 'reflect-metadata';
-import express, { json } from 'express';
+import express, { json, Request, Response } from 'express';
+import "express-async-errors";
 import  createConnection from "./database";
 import { router } from './routes';
+import { NextFunction } from 'express';
+import { AppError } from './errors/AppError';
 
 createConnection();
 const app = express();
 
 app.use(json());
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if(err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message: `Internal server error ${err.message}`
+    })
+})
 
 export { app };
